142fa1bb365880a2c38b70de4b028887ce7c85da
Jillian Armonia <jillian.marice@gmail.com>
2024-05-31 15:13:12 +0900

    finished cursor.js logic

--- starter/class/cursor.js
+++ starter/class/cursor.js
@@ -22,12 +22,30 @@ class Cursor {
     Screen.setBackgroundColor(this.row, this.col, this.cursorColor);
   }
 
+  down(){
+    if (this.row < this.numRows - 1 && this.row >= 0){
+      this.row++;
+    }
+  }
+
+  up(){
+    if (this.row < this.numRows && this.row > 0){
+      this.row--;
+    }
+  }
+
   left() {
     // Move cursor left
+    if (this.col < this.numCols && this.col > 0){
+      this.col--;
+    }
   }
 
   right() {
     // Move cursor right
+    if (this.col < this.numCols - 1 && this.col >= 0){
+      this.col++;
+    }
   }
 
 }
